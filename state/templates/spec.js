7a264a571a6021377c2287ba5b76f24a9ddc3ab0
Mato Ilic <info@matoilic.ch>
2015-09-12 16:53:36 +0200

    better spec descriptions

--- state/templates/spec.js
+++ state/templates/spec.js
@@ -2,7 +2,7 @@ import angular from 'angular';
 import 'angular-mocks';
 import component from './index';
 
-describe('<%= _.titleize(componentName) %>', function() {
+describe('<%= _.titleize(_.humanize(componentName)) %>', function() {
     beforeEach(angular.mock.module(component.name));
 
     it('should pass the dummy test to verify the karma setup', function() {
