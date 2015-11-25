b6b6e4d53c22645485629c0baec9a1db3e19b7d5
Mato Ilic <mat@ti8m.ch>
2015-11-25 11:17:52 +0100

    catch errors during lazy loading of states

--- app/templates/core/src/components/application/config/routing.js
+++ app/templates/core/src/components/application/config/routing.js
@@ -2,15 +2,14 @@ import futureRoutes from './routes.json!';
 
 function routingConfig($locationProvider, $urlRouterProvider, $httpProvider, $futureStateProvider) {
     $futureStateProvider.stateFactory('load', ['$q', '$ocLazyLoad', 'futureState', function($q, $ocLazyLoad, futureState) {
-        const def = $q.defer();
-
-        System.import(futureState.src).then(loadedModule => {
-            $ocLazyLoad.inject(loadedModule.name || loadedModule.default.name || loadedModule).then(function() {
-                def.resolve();
-            });
-        });
-
-        return def.promise;
+        return System
+            .import(futureState.src)
+            .then(loadedModule => {
+                return $ocLazyLoad.inject(loadedModule.name || loadedModule.default.name || loadedModule);
+            })
+            // this needs to be done so that the future state handler doesn't use the component name as state name
+            .then(() => null)
+            .catch(console.error.bind(console));
     }]);
 
     futureRoutes.forEach(function(r) {
