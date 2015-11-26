0cab5847cacf1186650ae64ad3403f7299f866f7
Mato Ilic <mat@ti8m.ch>
2015-11-26 15:27:35 +0100

    don't bootstrap the application manually by default to ease testing

--- app/templates/core/config/protractor.js
+++ app/templates/core/config/protractor.js
@@ -35,11 +35,5 @@ module.exports.config = {
     maxSessions: 1,
     jasmineNodeOpts: {
         defaultTimeoutInterval: 360000
-    },
-    onPrepare: function() {
-        //this is required because we manually bootstrap angular
-        browser.manage().timeouts().pageLoadTimeout(400000);
-        browser.manage().timeouts().implicitlyWait(250000);
-        browser.ignoreSynchronization = true;
     }
 }
--- app/templates/core/src/index.js
+++ app/templates/core/src/index.js
@@ -1,9 +1,2 @@
 import 'babel/external-helpers';
-import angular from 'angular';
-import appModule from './components/application/index';
-
-angular.element(document).ready(function() {
-    var appContainer = document.querySelector('#applicationContainer');
-    appContainer.setAttribute('ui-view', 'page');
-    angular.bootstrap(appContainer, [appModule.name], {strictDi: true});
-});
+import 'components/application/index';
