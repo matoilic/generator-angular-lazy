14e59a6f475fe3a4a163379c95e92bf6e8ca11c4
Samar Rizvi <samarrizvi.lko@gmail.com>
2016-11-01 18:25:43 +0530

    Update watch.js
    Added Compile source for html so that whenever a physical template html file changes, the resulting template is automatically embedded into where templateURL is used.

--- app/templates/core/gulp-tasks/watch.js
+++ app/templates/core/gulp-tasks/watch.js
@@ -4,6 +4,7 @@ module.exports = (gulp, config, sync) => {
     gulp.task('watch', () => {
         gulp.watch(config.paths.stylesheets, sync.sync(['compile-stylesheets', 'notify-recompiled']));
         gulp.watch(config.paths.scripts, sync.sync(['compile-source', 'eslint', 'notify-recompiled']));
+        gulp.watch(config.paths.html, sync.sync(['compile-source', 'notify-recompiled']));
         gulp.watch(config.paths.html, sync.sync(['copy-static', 'htmlhint', 'notify-recompiled']));
         gulp.watch(config.paths.static, sync.sync(['copy-static', 'notify-recompiled']));
     });
